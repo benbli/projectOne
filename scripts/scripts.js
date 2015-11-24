e9c48e2e313ecf7374b1732592d932db491452e6
Ben Li <benbli@gmail.com>
2015-11-24 11:35:36 -0500

    adds working table bounce with semi-paddles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,24 +3,37 @@ console.log("loaded");
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var player;
+// var playerTwo;
 var ai;
 var ball;
 var upArrow = 38;
 var downArrow = 40;
-var spaceBar = 32;
-var score = 0;
+var wKey = 87;
+var sKey = 83;
+// var spaceBar = 32; //later version 'action key'
+var points = 0;
 var lives = 3;
-
+var startBtn = {};
+var restartBtn = {};
+var over = 0;
+//update with later versions//
+// var particlesCount = 20;
+// var flag = 0;
+// var particles = [];
+// var particlePos = {};
+// var multiplier = 1;
+// collision = document.getElementById("collide");
+//-----------------------//
 player = {
   x : null,
   y : null,
-  width : 20,
-  height : 100,
+  width : 10,
+  height : 80,
   update : function() {
-    if (keystate[upArrow]) {
+    if (keystate[wKey]) {
       this.y -= 7;
     }
-    if (keystate[downArrow]) {
+    if (keystate[sKey]) {
       this.y += 7;
     }
   },
@@ -34,9 +47,16 @@ player = {
 ai = {
   x : null,
   y : null,
-  width : 20,
-  height : 100,
-  update : function() {},
+  width : 10,
+  height : 80,
+  update : function() {
+    if (keystate[upArrow]) {
+      this.y -= 7;
+    }
+    if (keystate[downArrow]) {
+      this.y += 7;
+    }
+  },
   draw: function() {
     ctx.beginPath();
     ctx.fillRect(this.x, this.y, this.width, this.height)
@@ -47,9 +67,9 @@ ai = {
 ball = {
   x : null,
   y : null,
-  g : 0.3, //gravity
+  g : 0.1, //gravity
   vel : null, //velocity
-  fac : 0.8, //velocity reduction per bounce
+  fac : .8, //factor for changing ball velocity
   speed : 10,
   radius : 6,
   width : 600,// *placeholder: change the ball init
@@ -57,22 +77,34 @@ ball = {
   update : function() {
     //update gravity
     this.vel.y += this.g;
+
     //update position
     this.x += this.vel.x;
     this.y += this.vel.y;
-    //handles bounce
-    if (this.y + this.vel.y > canvas.height - this.radius) {
-      this.y = (canvas.height-this.radius);
-      this.vel.y *= -this.fac;
-    };
-    //boundary limitations
-    if (this.x + this.vel.x > canvas.width-this.radius) {
+
+    //bounce off table
+    if(this.y > 330) {
+      this.vel.y = -this.vel.y;
+    }
+
+    //increase ball speed
+    // if (player paddle is hit x4 = increase vel)
+    //   this.vel.y *= this.fac;
+
+    //left side boundary limitations
+    if (this.x + this.vel.x < player.width + this.radius) {
+      // if(this.y <  )
+        this.vel.x = -this.vel.x;
+    }
+
+    //right side boundary limitations
+    if (this.x + this.vel.x > (canvas.width - this.radius) - ai.width) {
       this.vel.x = -this.vel.x;
     }
   },
   draw : function() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false); //circle formula is 2*PI*R
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
     ctx.fillStyle = '#000';
     ctx.fill();
     ctx.closePath();
@@ -122,13 +154,12 @@ function update() {
 function draw() {
   ctx.clearRect(0,0,canvas.width, canvas.height);
   ctx.save();
-
   ball.draw();
   player.draw();
   ai.draw();
   ctx.restore();
-
-  //boundary limitations
-
 }
 main();
+
+//hit path, and a miss path
+//when AI gets harder, increase times of hit path, decrease times of miss
